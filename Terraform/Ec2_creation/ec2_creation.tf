0668618b08bc73ad07151aaa231a383584374569
Ganesh babu Soma <36077023+Somaganeshbabu@users.noreply.github.com>
2024-03-05 06:04:38 +0000

    basic of ec2 creation using terraform

---
+++ Terraform/Ec2_creation/ec2_creation.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+    region = "ap-south-1"
+    
+}
+
+resource "aws_instance" "aws_ec2_instance_name" {
+    ami = "ami-0e670eb768a5fc3d4"
+    instance_type = "t2.micro"
+    
+  
+}
