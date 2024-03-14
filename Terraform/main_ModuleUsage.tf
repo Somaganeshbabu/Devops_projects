bec9b7e9b0b1031bd4120f2faad0a75cb6a73c3d
Ganesh babu Soma <36077023+Somaganeshbabu@users.noreply.github.com>
2024-03-14 06:21:50 +0000

    statefile will be stored in s3 bucket from now on.

--- Terraform/main_ModuleUsage.tf
+++ Terraform/main_ModuleUsage.tf
@@ -1,10 +1,15 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
 # if you wnat to make use of defaults values 
 # written in ec2_variables.tf you don't have to
 # explicitly call them below, it will automatically take those default values
 module "ec2_instance" {
   source = "./modules/Ec2_creation"
+}
 
+resource "aws_s3_bucket" "bucket_creation" {
+  bucket = "statefiles-storage"
+  
 }
--- Terraform/modules/Ec2_creation/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mybucket"
-    key    = "statefiles/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
